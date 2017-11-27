f572a714043c73365a2952089f50b0aa0d7f8a8a
Tanya <vyrodova.tanya@gmail.com>
2017-11-27 22:07:00 +0300

    Add new menu item

--- js/map.js
+++ js/map.js
@@ -4,12 +4,16 @@
 var mapOpen = document.querySelector(".js-open-map");
 var mapPopup = document.querySelector(".modal-content-map");
 var mapClose = mapPopup.querySelector(".modal-content-close");
+var mapOverlay = document.querySelector(".modal-overlay");
 
 mapOpen.addEventListener("click", function(event) {
     event.preventDefault();
     mapPopup.classList.add("modal-content-show");
 });
-
+mapOpen.addEventListener("click", function(event) {
+    event.preventDefault();
+    mapOverlay.classList.add("modal-overlay-block");
+});
 mapClose.addEventListener("click", function(event) {
     event.preventDefault();
     mapPopup.classList.remove("modal-content-show");
