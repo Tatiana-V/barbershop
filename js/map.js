bf27af7a345aa31c85c3d12637d8ea918397dca1
Tanya <vyrodova.tanya@gmail.com>
2017-11-27 21:14:13 +0300

    Add new menu item

---
+++ js/map.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Tanya on 27.11.2017.
+ */
+var mapOpen = document.querySelector(".js-open-map");
+var mapPopup = document.querySelector(".modal-content-map");
+var mapClose = mapPopup.querySelector(".modal-content-close");
+
+mapOpen.addEventListener("click", function(event) {
+    event.preventDefault();
+    mapPopup.classList.add("modal-content-show");
+});
+
+mapClose.addEventListener("click", function(event) {
+    event.preventDefault();
+    mapPopup.classList.remove("modal-content-show");
+});
+
+window.addEventListener("keydown", function(event) {
+    if (event.keyCode === 27) {
+        if (mapPopup.classList.contains("modal-content-show")) {
+            mapPopup.classList.remove("modal-content-show");
+        }
+    }
+});
